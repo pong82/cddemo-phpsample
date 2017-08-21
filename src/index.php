988ee360fce823f0afeaa8e825b3e1392b2a5c4e
Paul Ong <Paul@Pauls-MacBook-Air.local>
2017-08-21 22:52:57 +1000

    new version

--- src/index.php
+++ src/index.php
@@ -1,3 +1,8 @@
-<?php
-echo "Hello world"
-?>
+<html>
+    <head>
+        <meta http-equiv="cache-control" content="no-cache">       
+    </head>
+    <body>
+        <?php echo "Hello World 2" ?>
+    </body>
+</html>
